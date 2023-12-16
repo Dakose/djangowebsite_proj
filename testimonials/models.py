67c67e5e572501086b640a5edbbadb786004adae
Bohdan Holubnychyi <bohdanholubn@gmail.com>
2023-12-16 14:37:41 +0200

    fixed

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
 class TestimonialsSlider(models.Model):
-    img = models.ImageField(upload_to='testimonialsimage/')
+    img = models.ImageField(upload_to='testimonialsimage/', validators=[FileExtensionValidator(['jpg', 'pdf', 'doc', 'svg'])])
     title = models.CharField(max_length=255, verbose_name='Testimonial title')
     text = models.CharField(max_length=255, verbose_name='Testimonial text')
     css = models.CharField(max_length=255, null=True, default='-', verbose_name='Testimonial css')
